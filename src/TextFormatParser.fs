927b3cb1eb0ff984af5bd82c87a49a6a13eebadc
Mark Shevchenko <mark-progmsk@yandex-team.ru>
2022-09-05 18:54:25 +0300

    Implement floatLit rule

--- src/TextFormatParser.fs
+++ src/TextFormatParser.fs
@@ -14,3 +14,11 @@ let identifier : Parser<string, unit> =
 
 let decLit : Parser<string, unit> = notFollowedBy (pstring "0") >>. (many1Satisfy isDigit)
                                 <|> pstring "0" .>> notFollowedBy digit
+let exp : Parser<string, unit> = (pstringCI "e+" <|> pstringCI "e-" <|> pstringCI "e")
+                             .>>. many1Satisfy isDigit |>> (fun (e, digits) -> e + digits)
+
+let floatLit : Parser<string, unit> = (pstring "." .>>. many1Satisfy isDigit .>>. opt exp
+                                       |>> (fun ((dot, digits), exp) -> dot + digits + Option.defaultValue "" exp))
+                                  <|> (decLit .>>. exp |>> (fun (digits, exp) -> digits + exp))
+                                  <|> (decLit .>>. pstring "." .>>. (manySatisfy isDigit) .>>. opt exp
+                                       |>> (fun (((d1, dot), d2), exp) -> d1 + dot + d2 + Option.defaultValue "" exp))
--- tests/TextFormatParserTests.fs
+++ tests/TextFormatParserTests.fs
@@ -7,65 +7,111 @@ open TextFormat
 module Assert =
     open FParsec
     
-    let Parse<'r>(parser: Parser<'r, unit>, source: string) =
+    let Parse<'r>(source: string, parser: Parser<'r, unit>) =
         match run parser source with
         | Success (result, _, _) -> Assert.True(true, sprintf $"%A{result}")
         | Failure (message, _, _) -> Assert.True(false, message)
         
-    let ParseEqual<'r>(expected: 'r, parser: Parser<'r, unit>, source: string) =
+    let ParseEqual<'r>(source: string, parser: Parser<'r, unit>, expected: 'r) =
         match run parser source with
         | Success (result, _, _) -> Assert.Equal(expected, result)
         | Failure (message, _, _) -> Assert.True(false, message)
 
-    let NotParse<'r>(parser: Parser<'r, unit>, source: string) =
+    let NotParse<'r>(source: string, parser: Parser<'r, unit>) =
         match run parser source with
         | Success (result, _, _) -> Assert.True(false, sprintf $"%A{result}")
         | Failure (message, _, _) -> Assert.True(true, message)
 
 [<Fact>]
 let ``#123 is comment`` () =
-    Assert.Parse(comment, "#123")
+    Assert.Parse("#123", comment)
     
 [<Fact>]
 let ``# 123 is comment`` () =
-    Assert.Parse(comment, "# 123")
+    Assert.Parse("# 123", comment)
 
 [<Fact>]
 let ``# 123\n is comment`` () =
-    Assert.Parse(comment, "# 123\n")
+    Assert.Parse("# 123\n", comment)
     
 [<Fact>]
 let ``#\n is comment`` () =
-    Assert.Parse(comment, "#\n")
+    Assert.Parse("#\n", comment)
 
 [<Fact>]
 let ``\n is not comment`` () =
-    Assert.NotParse(comment, "\n")
+    Assert.NotParse("\n", comment)
     
 [<Fact>]
 let ``abc123 is identifier`` () =
-    Assert.Parse(identifier, "abc123")
+    Assert.Parse("abc123", identifier)
     
 [<Fact>]
 let ``aBc123 is identifier`` () =
-    Assert.Parse(identifier, "aBc123")
+    Assert.Parse("aBc123", identifier)
     
 [<Fact>]
 let ``ABC is identifier`` () =
-    Assert.Parse(identifier, "ABC")
+    Assert.Parse("ABC", identifier)
 
 [<Fact>]
 let ``123 is not identifier`` () =
-    Assert.NotParse(identifier, "123")
+    Assert.NotParse("123", identifier)
     
 [<Fact>]
 let ``123 is decimal literal"`` () =
-    Assert.Parse(decLit, "123")    
+    Assert.Parse("123", decLit)    
 
 [<Fact>]
 let ``0 is decimal literal`` () =
-    Assert.Parse(decLit, "0")    
+    Assert.Parse("0", decLit)    
 
 [<Fact>]
 let ``0123 is not decimal literal`` () =
-    Assert.NotParse(decLit, "0123")
+    Assert.NotParse("0123", decLit)
+    
+[<Fact>]
+let ``e+100 is exp`` () =
+    Assert.ParseEqual("e+100", exp, "e+100")
+        
+[<Fact>]
+let ``E-100 is exp`` () =
+    Assert.ParseEqual("E-100", exp, "E-100")
+    
+    
+[<Fact>]
+let ``e100 is exp`` () =
+    Assert.ParseEqual("e100", exp, "e100")
+    
+    
+[<Fact>]
+let ``e +100 is not exp`` () =
+    Assert.NotParse("e +100", exp)
+
+[<Fact>]
+let ``.123 is floatLit`` () =
+    Assert.Parse(".123", floatLit)
+
+[<Fact>]
+let ``.123e10 is floatLit`` () =
+    Assert.Parse(".123e10", floatLit)
+
+[<Fact>]
+let ``.123e is not floatLit`` () =
+    Assert.NotParse(".123e", floatLit)
+
+[<Fact>]
+let ``123. is floatLit`` () =
+    Assert.Parse("123.", floatLit)
+
+[<Fact>]
+let ``123.456 is floatLit`` () =
+    Assert.Parse("123.456", floatLit)
+
+[<Fact>]
+let ``123.456e78 is floatLit`` () =
+    Assert.Parse("123.456r78", floatLit)
+    
+[<Fact>]
+let ``123e78 is floatLit`` () =
+    Assert.Parse("123e78", floatLit)
