100782c38c70b5d9b4e76b3a0327a91bc9f8b724
Mark Shevchenko <markshevchenko@gmail.com>
2023-09-14 23:35:45 +0300

    Fix help text

--- ProtoS/Program.fs
+++ ProtoS/Program.fs
@@ -1,19 +1,18 @@
 ﻿module Program
 
 open System
-open Proto3
-open Proto3Parser
 
 let println (s: string) = Console.WriteLine(s)
 
 
-println "protos <proto3> --bin [--input=<file>] [--output=<file>]"
-println "protos <proto3> --text [--input=<file>] [--output=<file>]"
+println "protos <proto3> <message-name> --bin [--input=<file>] [--output=<file>]"
+println "protos <proto3> <message-name> --text [--input=<file>] [--output=<file>]"
 println "protos [--help]"
 println ""
 println "--bin, -b    -- serialize text input to binary output"
 println "--text, -t d -- deserialize binary input to text output"
-println "<proto3> is the description of the message."
+println "<proto3> is the .proto the"
+println "<message-name> is the message name"
 println ""
 println "-i=file, --input=file  -- input <file>, if missed, stdin used"
 println "-o=file, --output=file -- output <file>, if missed, stdout used"
